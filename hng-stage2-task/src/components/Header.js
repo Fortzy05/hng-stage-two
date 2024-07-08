2934f42c9511921662ba02f21d4bff87a6c5c9a4
fortune <omorodion.fortune@gmail.com>
2024-07-08 10:37:18 +0100

    header component

--- hng-stage2-task/src/components/Header.js
+++ hng-stage2-task/src/components/Header.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from "react";
+
 
 function Header() {
   return (
-    <header className="h-[128px] border relative border-black w-full">
-      <section className='flex items-center h-[80px]'>
-        <div className="text-2xl ">
-          <span className='absolute top-[5.92px] left-[5.67px]'>J</span>
-          <span className='absolute top'>T</span>
+    <header className="h-[128px] bg-[#f4f4f479] flex justify-between py-10 px-20 items-center w-full">
+      <section className="flex items-center h-[80px]">
+        <div className="pl-2 pr-20 flex items-center font-bold">
+          <img src="/logo.svg" />
         </div>
-        <div>
-          <h1>John Turner</h1>
-        </div>
+        <ul className="flex gap-[60px]">
+          <li className="hover:text-[#AC702F] flex items-center gap-1">
+            Brands
+            <span>
+              <img src="/down_arrow.svg" alt="arrow facing downward" />
+            </span>
+          </li>
+          <li className="hover:text-[#AC702F]">Inspirations</li>
+          <li className="hover:text-[#AC702F] flex gap-1 items-center">
+            Categories
+            <span>
+              <img src="/down_arrow.svg" alt="arrow facing downward" />
+            </span>
+          </li>
+          <li className="hover:text-[#AC702F]">Sale</li>
+        </ul>
       </section>
+
+      <div className="md:border md:w-[8rem] rounded-lg py-1 px-4 border-[#E2E8F0] bg-transparent flex items-center gap-3">
+        <img src="/search.svg" alt="search icon" className="md:w-5 w-8 " />
+        <input
+          className="w-full outline-none md:block hidden "
+          type="text"
+          placeholder="search"
+        />
+      </div>
     </header>
   );
 }
 
-export default Header
+export default Header;
